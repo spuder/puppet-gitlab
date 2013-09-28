67a5c796cfab9d3e6d7b0d08a32f1b25c3e441cc
Spuder <bubbleboy450@gmail.com>
2013-09-28 16:14:46 -0600

    Adds vagrant files to the vagrant branch

---
+++ boostrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update 
+
+sudo apt-get install -y python-software-properties
+cd /tmp
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+sudo dpkg -i puppetlabs-release-precise.deb
+apt-get update 
+apt-get install -y puppet-common
