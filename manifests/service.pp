8f349f5cb4ccf123d65b5ee6ddddb3c0dabdd1d4
Spuder <bubbleboy450@gmail.com>
2013-09-30 00:29:22 -0600

    Added 'default_server' to nginx config, prevents 502 error Disabled has status and has restart from gitlab service to possibly fix issue where you must restart gitlab after first puppet run (unverified)

--- manifests/service.pp
+++ manifests/service.pp
@@ -15,9 +15,22 @@ class gitlab::service inherits gitlab {
 	service { 'gitlab' :
 	  ensure    => running,
 	  enable    => true,
-	  hasrestart  => true,
-	  hasstatus   => true,  
+	  #hasrestart  => true,
+	  #hasstatus   => true,  
 	}
-	
+	#TODO: Figure out why puppet says gitlab was started, but doesnt' actually start.
+#	Notice: /Stage[main]/Gitlab::Service/File[/etc/init.d/gitlab]/ensure: defined content as '{md5}27aeb088ff6d8ed1e5d5952754d68bd7'
+#Debug: /Stage[main]/Gitlab::Service/File[/etc/init.d/gitlab]: The container Class[Gitlab::Service] will propagate my refresh event
+#Debug: Service[gitlab](provider=upstart): Could not find gitlab.conf in /etc/init
+#Debug: Service[gitlab](provider=upstart): Could not find gitlab.conf in /etc/init.d
+#Debug: Service[gitlab](provider=upstart): Could not find gitlab in /etc/init
+#Debug: Executing '/etc/init.d/gitlab status'
+#Debug: Executing '/usr/sbin/update-rc.d -f gitlab remove'
+#Debug: Executing '/usr/sbin/update-rc.d gitlab defaults'
+#Notice: /Stage[main]/Gitlab::Service/Service[gitlab]/enable: enable changed 'false' to 'true'
+#Debug: /Stage[main]/Gitlab::Service/Service[gitlab]: The container Class[Gitlab::Service] will propagate my refresh event
+#Debug: Class[Gitlab::Service]: The container Stage[main] will propagate my refresh event
+#Debug: Finishing transaction 69953226426760
+#Debug: Storing state
 	
 }
