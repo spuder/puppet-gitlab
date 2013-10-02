bcc79eeba3de2f27f2fef716ad0be70c5d5a23e2
Spuder <bubbleboy450@gmail.com>
2013-10-02 10:12:16 -0600

    Remove $gitlab_username_change (now called $user_changename) reformats parameter order fro readability

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,15 @@
-# Class: gitlab
-#
-# This module manages gitlab
-#
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
-#
-# Sample Usage:
-#
 class gitlab (
+    #Gitlab server settings
+    $gitlab_branch          = $gitlab::params::gitlab_branch,
+    $gitlabshell_branch     = $gitlab::params::gitlabshell_branch,
     $git_user               = $gitlab::params::git_user,
     $git_home               = $gitlab::params::git_home,
     $git_email              = $gitlab::params::git_email,
     $git_comment            = $gitlab::params::git_comment,
     $gitlab_sources         = $gitlab::params::gitlab_sources,
-    $gitlab_branch          = $gitlab::params::gitlab_branch,
-    $gitlabshell_branch     = $gitlab::params::gitlabshell_branch,
     $gitlabshell_sources    = $gitlab::params::gitlabshell_sources,
     
+    #Database
     $gitlab_dbtype          = $gitlab::params::gitlab_dbtype,
     $gitlab_dbname          = $gitlab::params::gitlab_dbname,
     $gitlab_dbuser          = $gitlab::params::gitlab_dbuser,
@@ -29,13 +19,13 @@ class gitlab (
     $gitlab_domain          = $gitlab::params::gitlab_domain,
     $gitlab_repodir         = $gitlab::params::gitlab_repodir,
     
+    #Web & Security
     $gitlab_ssl             = $gitlab::params::gitlab_ssl,
     $gitlab_ssl_cert        = $gitlab::params::gitlab_ssl_cert,
     $gitlab_ssl_key         = $gitlab::params::gitlab_ssl_key,
     $gitlab_ssl_self_signed = $gitlab::params::gitlab_ssl_self_signed,
-    $gitlab_projects        = $gitlab::params::gitlab_projects,
-    #$gitlab_username_change = $gitlab::params::gitlab_username_change,
     
+    #LDAP
     $ldap_enabled           = $gitlab::params::ldap_enabled,
     $ldap_host              = $gitlab::params::ldap_host,
     $ldap_base              = $gitlab::params::ldap_base,
@@ -45,20 +35,24 @@ class gitlab (
     $ldap_bind_dn           = $gitlab::params::ldap_bind_dn,
     $ldap_bind_password     = $gitlab::params::ldap_bind_password,
     
+    #Company Branding
     $custom_login_icon      = $gitlab::params::custom_login_icon,#TODO:Rename to logo
     $custom_thumbnail_icon  = $gitlab::params::custom_thumbnail_icon,#TODO:Rename to logo
     
+    #User default settings
     $gitlab_gravatar        = $gitlab::params::gitlab_gravatar,
     $user_create_group      = $gitlab::params::user_create_group,
     $user_create_team       = $gitlab::params::user_create_team,
     $user_changename        = $gitlab::params::user_changename,
     
-    #Default Project features
+    #Project default features
     $project_issues         = $gitlab::params::project_issues,
     $project_merge_request  = $gitlab::params::project_merge_request,
     $project_wiki           = $gitlab::params::project_wiki,
     $project_wall           = $gitlab::params::project_wall,
     $project_snippets       = $gitlab::params::project_snippets,
+    $gitlab_projects        = $gitlab::params::gitlab_projects,
+    
     
     
   ) inherits gitlab::params {
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,17 @@
 #init -> packages -> user -> setup -> install -> config -> service
 class gitlab::params {
   
-  $git_user               = 'git'
+  #Gitlab server settings
+  $gitlab_branch          = '6-0-stable'
+  $gitlabshell_branch     = 'v1.7.1'
+  $git_user               = 'git'       #only change if you really know what you are doing
   $git_home               = '/home/git'
   $git_email              = 'git@someserver.net'
   $git_comment            = 'GitLab'
   $gitlab_sources         = 'git://github.com/gitlabhq/gitlabhq.git'
-  $gitlab_branch          = '6-0-stable'
   $gitlabshell_sources    = 'git://github.com/gitlabhq/gitlab-shell.git'
-  $gitlabshell_branch     = 'v1.7.1'
   
+  #Database
   $gitlab_dbtype          = 'mysql'
   $gitlab_dbname          = 'gitlabdb'
   $gitlab_dbuser          = 'gitlabdbu'
@@ -19,22 +21,23 @@ class gitlab::params {
   $gitlab_domain          = $::fqdn
   $gitlab_repodir         = $git_home
   
+  #Web & Security
   $gitlab_ssl             = false
   $gitlab_ssl_cert        = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
   $gitlab_ssl_key         = '/etc/ssl/private/ssl-cert-snakeoil.key'
   $gitlab_ssl_self_signed = false
-  $gitlab_projects        = '10'
-  #$gitlab_username_change = true
   
+  #LDAP 
   $ldap_enabled           = false
   $ldap_host              = 'ldap.domain.com'
   $ldap_base              = 'dc=domain,dc=com'
-  $ldap_uid               = 'uid'
+  $ldap_uid               = 'uid' #Active directory = 'sAMAccountName'
   $ldap_port              = '636'
-  $ldap_method            = 'ssl'
+  $ldap_method            = 'ssl' 
   $ldap_bind_dn           = ''
   $ldap_bind_password     = ''
   
+  #Company Branding
   $custom_login_icon      = false
   $custom_thumbnail_icon  = false
   
@@ -50,29 +53,9 @@ class gitlab::params {
   $project_wiki           = true
   $project_wall           = false
   $project_snippets       = false
+  $gitlab_projects        = '10'
  
 
 }
 
-## EXTRA MODULE INTERNAL VARIABLES
-#(add here module specific internal variables)
-
-## MODULE INTERNAL VARIABLES
-# (Modify to adapt to unsupported OSes)
-
-## DEFAULTS FOR MONITOR CLASS
-# These are settings that influence the (optional) foo::monitor class
-# You can define these variables or leave the defaults
-# The apparently complex variables assignements below follow this logic:
-# - If no user variable is set, a reasonable default is used
-# - If the user has set a host-wide variable (ex: $monitor_target ) that one is set
-# - The host-wide variable can be overriden by a module specific one (ex: $foo_monitor_target)
-
-
-## FILE SERVING SOURCE
-# Sets the correct source for static files
-# In order to provide files from different sources without modifying the module
-# you can override the default source path setting the variable $base_source
-# Ex: $base_source="puppet://ip.of.fileserver" or $base_source="puppet://$servername/myprojectmodule"
-# What follows automatically manages the new source standard (with /modules/) from 0.25 
 
