47f8cfc59f33c7d5cbe4f43e63312c1e4b0ea3f4
spuder <bubbleboy450@gmail.com>
2013-10-13 22:43:43 -0600

    Changes test/init.pp default to use non standard icons

--- tests/init.pp
+++ tests/init.pp
@@ -46,9 +46,9 @@
 	  ldap_bind_dn           => '',
 	  ldap_bind_password     => '',
 	  
-	  use_custom_login_logo  => false,
-	  use_custom_thumbnail   => false,
-	  use_company_link       => false,
+	  use_custom_login_logo  => true,
+	  use_custom_thumbnail   => true,
+	  use_company_link       => true,
 	  company_link           => 'http://failblog.cheezburger.com',
 	  project_public_default => false,
 	  
