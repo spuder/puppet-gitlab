55a3009e64ebbe4b4c1e3d8e7f80d9a39fa71009
Spencer <bubbleboy450@gmail.com>
2013-10-20 12:20:18 -0600

    Indentation

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,32 +2,21 @@
 class gitlab::packages inherits gitlab {
 
 	$system_packages = [
-											'libicu-dev',
-											'python2.7',
-											'python-docutils',
-						          'libxml2-dev',
-						          'libxslt1-dev',
-	                    'python-dev',
-	                    'build-essential',
-	                    'libmysqlclient-dev',
-	                    'redis-server',
-	                    'nginx', #TODO, decide if nginx should be installed via apt, or puppet (pupet had some problems)
+			'libicu-dev',
+			'python2.7',
+			'python-docutils',
+			'libxml2-dev',
+			'libxslt1-dev',
+	                'python-dev',
+	                'build-essential',
+	                'libmysqlclient-dev',
+	                'redis-server',
+	                'nginx', #TODO, decide if nginx should be installed via apt, or puppet (pupet had some problems)
 	                    ]
-
-                          
+  
 	package { $system_packages: 
 	  ensure => present,
 	}
-
-
-	
-#	 #Run apt-get update after repo install
-#  exec { 'apt-get update':
-#    path => '/usr/bin',
-#    command => 'apt-get update',
-#  } #TODO: use puppetlabs-apt always update resource to manage this
-#  
-#  
   
 	
 	## Git v1.7.10 
@@ -35,17 +24,14 @@ class gitlab::packages inherits gitlab {
 	
 	include apt
 
-  #Include git ppa (gitlab requires git 1.7.10 or newer which isn't in standard repo)
-	apt::ppa { 'ppa:git-core/ppa': 
-#	  notify => Exec['apt-get update'],
+         #Include git ppa (gitlab requires git 1.7.10 or newer which isn't in standard repo)
+	apt::ppa { 'ppa:git-core/ppa':
 	}
 	
-	#Install key for repo (otherwise it prints error)
-	
+	 #Install key for repo (otherwise it prints error)
 	apt::key { 'ppa:git-core/ppa': 
 	    key    => 'E1DF1F24',
 	}
-	
 	
 	package { 'git-core': 
 	  ensure => present, 
@@ -53,12 +39,9 @@ class gitlab::packages inherits gitlab {
 	              Apt::Ppa['ppa:git-core/ppa'],
 	              Apt::Key['ppa:git-core/ppa'],
 	              ],
-  }
+        }
 	
-	
-	
-	
-	
+
 	## Ruby
 	#=====================================
 	 
@@ -82,9 +65,6 @@ class gitlab::packages inherits gitlab {
 	  
 	}
 
-  
-
-	
 	
 	## Nginx
 	#=====================================
@@ -93,24 +73,24 @@ class gitlab::packages inherits gitlab {
 	
 	
 	## MySQL
-  #=====================================
+        #=====================================
   
-  #include mysql #This syntax is no longer supported as of puppetlabs-mysql 2.0.0
-  # The end user must manually setup the database, see tests/init.pp for an example
+        #include mysql #This syntax is no longer supported as of puppetlabs-mysql 2.0.0
+        # The end user must manually setup the database, see tests/init.pp for an example
 
-  mysql::db { "$gitlab::gitlab_dbname" :
-  ensure    => present,
+         mysql::db { "$gitlab::gitlab_dbname" :
+          ensure    => present,
 	  user      => "$gitlab::gitlab_dbuser",
 	  password  => "$gitlab::gitlab_dbpwd",
 	  host      => "$gitlab::gitlab_dbhost",
 	  grant     => ['all'],
 	  #TODO: change from all, to just the permissions specififed in the install doc
-  }
+        }
   
   
-  ## Postfix
-  #===================================
+        ## Postfix
+        #===================================
   
-  include postfix
+        include postfix
   
 }#end packages.pp
