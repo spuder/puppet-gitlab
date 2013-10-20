9fea413b48e97553c050f724f6fabc61a192f5bd
spuder <bubbleboy450@gmail.com>
2013-10-20 17:33:56 -0600

    Fixes login for git user http://unix.stackexchange.com/questions/96892/what-does-adduser-disabled-login-do/96897#96897

--- manifests/user.pp
+++ manifests/user.pp
@@ -4,11 +4,10 @@ class gitlab::user inherits gitlab {
   user { "${gitlab::git_user}":
     ensure      => present,
     name        => "${gitlab::git_user}",
-    #shell       => '/bin/false',      #https://github.com/gitlabhq/gitlabhq/pull/5218
     shell       => '/bin/bash',
-    password    => '*',
+    password    => '!',                 #same as --disabled-login
     home        => "${gitlab::git_home}",
-    system      => true,                   #Makes sure user has uid less than 500 (or 1000)
+#    system      => true,                   #Makes sure user has uid less than 500 (or 1000)
     managehome  => true,
     comment     => "${gitlab::git_comment}", #Same as --gecos
   }
