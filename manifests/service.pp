519d2f20d455c34cc6c3608814a0387c6aeb45e5
Spencer <bubbleboy450@gmail.com>
2013-10-20 12:23:55 -0600

    Indentation

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,20 +11,20 @@ class gitlab::service inherits gitlab {
   }
   
   #gitlab service
-	service { 'gitlab' :
-	  ensure    => running,
-	  enable    => true,
-	  hasrestart  => true,
-	  #hasstatus   => true, #https://ask.puppetlabs.com/question/3382/starting-service-fails/
-	  status  => '/etc/init.d/gitlab status | /bin/grep -q "up and running"',
-	  subscribe    => File['/etc/init.d/gitlab'],  
-	}
+  service { 'gitlab' :
+    ensure       => running,
+    enable       => true,
+    hasrestart   => true,
+    #hasstatus   => true, #https://ask.puppetlabs.com/question/3382/starting-service-fails/
+    status  => '/etc/init.d/gitlab status | /bin/grep -q "up and running"',
+    subscribe    => File['/etc/init.d/gitlab'],  
+  }
 
-	service { 'nginx' :
-	  ensure   => running,
-	  enable   => true,    
-	  hasrestart  => true,
+  service { 'nginx' :
+    ensure      => running,
+    enable      => true,    
+    hasrestart  => true,
     hasstatus   => true, 
     require     => Service ['gitlab'],
-	}
+  }
 }
