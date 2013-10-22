6f433d617baf976d56a27efe046e799dfc0d9a79
spuder <bubbleboy450@gmail.com>
2013-10-22 11:39:24 -0600

    Changes gitlab port number from 80 to either 80 or 443 depending on variable Updates upgrade-checklist Updates test.pp from 6-1 to 6-2

--- tests/init.pp
+++ tests/init.pp
@@ -18,7 +18,7 @@
 	  git_email              => 'git@foo.com',
 #	  git_comment            => 'GitLab',
 #	  gitlab_sources         => 'git://github.com/gitlabhq/gitlabhq.git',
-	  gitlab_branch          => '6-1-stable',
+	  gitlab_branch          => '6-2-stable',
 #	  gitlabshell_sources    => 'git://github.com/gitlabhq/gitlab-shell.git',
 	  gitlabshell_branch     => 'v1.7.1',
 	  
