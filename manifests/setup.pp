16ee6cdef2134b2f1dfb208a7156e56f77a433e4
Daniel Lawrence <dannyla@linux.com>
2014-01-08 17:00:34 +1100

    removed charlock holmes notify for every puppet run

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -22,8 +22,6 @@
                   ],
   }
   
-  notify { "Installing charlock holmes with ruby version ${::rubyversion}" : }
-  
   #Execute all commands as the git user in the git home directory
   Exec {
     user    =>  "${gitlab::git_user}",
