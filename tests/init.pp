03d104b98e195f60c31db3f60cb61a79ee6b4535
Spencer Owen <owenspencer@gmail.com>
2014-06-24 18:29:24 -0600

    Deletes all old files

--- tests/init.pp
+++ tests/init.pp
@@ -1,67 +1,53 @@
-#This is the syntax to create a mysql server in puppetlabs-mysql version < 2.0.0
-#  class { 'mysql::server':
-#    config_hash => { 'root_password' => 'badpassword' }
-#  }
 
-#This is the syntax to create a mysql server in puppetlabs-mysql version > 2.0.0
-#Use caution as it could overwrite an existing database
-  class { 'mysql::server':
-    root_password   => 'foo', 
-  }
-
-  
   class { 'gitlab' : 
-    gitlab_manage_packages => true,
     # git_user               => 'git',
     # git_home               => '/home/git',
-    git_email              => 'git@foo.com',
+    # git_email              => 'git@foo.com',
     # git_comment            => 'GitLab',
-    git_ssh_port           => '22',
+    # git_ssh_port           => '22',
     # gitlab_sources         => 'git://github.com/gitlabhq/gitlabhq.git',
-    gitlab_branch          => '6-9-stable',
+    gitlab_branch          => '7.0.0',
     # gitlabshell_sources    => 'git://github.com/gitlabhq/gitlab-shell.git',
-    gitlabshell_branch     => 'v1.9.4',
+    # gitlabshell_branch     => 'v1.9.4',
     
-    gitlab_dbtype          => 'mysql',
-    gitlab_dbname          => 'gitlabdb',
-    gitlab_dbuser          => 'gitlabdbu',
-    gitlab_dbpwd           => 'changeme',
-    gitlab_dbhost          => 'localhost',
-    gitlab_dbport          => '3306',  
-    gitlab_ssl             => true,
-    gitlab_ssl_cert        => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-    gitlab_ssl_key         => '/etc/ssl/private/ssl-cert-snakeoil.key',
-    gitlab_ssl_self_signed => true, #Do not use self signed certs in production!
-    gitlab_projects        => '15',
-    default_servername     => 'gitlab', #If you want to change gitlab.foo.com to foobar.foo.com
-    # gitlab_username_change => true,
+    # gitlab_dbname          => 'gitlabdb',
+    # gitlab_dbuser          => 'gitlabdbu',
+    # gitlab_dbpwd           => 'changeme',
+    # gitlab_dbhost          => 'localhost',
+    # gitlab_dbport          => '3306',  
+    # gitlab_ssl             => true,
+    # gitlab_ssl_cert        => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    # gitlab_ssl_key         => '/etc/ssl/private/ssl-cert-snakeoil.key',
+    # gitlab_ssl_self_signed => true, #Do not use self signed certs in production!
+    # gitlab_projects        => '15',
+    # default_servername     => 'gitlab', #If you want to change gitlab.foo.com to foobar.foo.com
+    # # gitlab_username_change => true,
     
-    ldap_enabled           => false,
-    ldap_host              => 'ldap.domain.com',
-    ldap_base              => 'dc=domain,dc=com',
-    ldap_uid               => 'uid',
-    ldap_port              => '636',
-    ldap_method            => 'ssl',
-    ldap_bind_dn           => '',
-    ldap_bind_password     => '',
+    # ldap_enabled           => false,
+    # ldap_host              => 'ldap.domain.com',
+    # ldap_base              => 'dc=domain,dc=com',
+    # ldap_uid               => 'uid',
+    # ldap_port              => '636',
+    # ldap_method            => 'ssl',
+    # ldap_bind_dn           => '',
+    # ldap_bind_password     => '',
     
-    use_custom_login_logo  => true,
-    use_custom_thumbnail   => false,
-    use_company_link       => true,
-    company_link           => 'http://failblog.cheezburger.com',
-    # project_public_default => false, #Deprecated
-    visibility_level       => 'internal', # New in 6-4, replaces project_public_default
+    # use_custom_login_logo  => true,
+    # use_custom_thumbnail   => false,
+    # use_company_link       => true,
+    # company_link           => 'http://failblog.cheezburger.com',
+    # visibility_level       => 'internal', # New in 6-4, replaces project_public_default
     
-    gitlab_gravatar        => true,
-    user_create_group      => false,
-    user_changename        => false,
+    # gitlab_gravatar        => true,
+    # user_create_group      => false,
+    # user_changename        => false,
     
-    #Default Project features
-    project_issues         => true,
-    project_merge_request  => true,
-    project_wiki           => false,
-    project_wall           => false,
-    project_snippets       => true,
+    # #Default Project features
+    # project_issues         => true,
+    # project_merge_request  => true,
+    # project_wiki           => false,
+    # project_wall           => false,
+    # project_snippets       => true,
     
     }
     
--- tests/ruby20.pp
+++
@@ -1,6 +0,0 @@
-  class { 'ruby':
-    ruby_package      =>  'ruby1.9.1-full',
-    rubygems_package  =>  'rubygems1.9.1',
-    rubygems_update   =>  true,
-    gems_version      =>  'latest',
-  }
