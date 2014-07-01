88de69376c36a593c3bb3dc60464e08ab1a65c85
Spencer Owen <owenspencer@gmail.com>
2014-07-01 17:44:02 -0600

    Adds travis tests according to http://puppetlabs.com/blog/the-next-generation-of-puppet-module-testing

---
+++ spec/classes/gitlab_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe 'gitlab', :type => 'class' do
+
+  context "On a Debian OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'Debian'
+      }
+    end
+
+    it {
+      should contain_package('gitlab').with( { 'name' => 'gitlab' } )
+      should contain_service('gitlab').with( { 'name' => 'gitlab' } )
+    }
+  end
+
+  context "On a RedHat OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it {
+      should contain_package('gitlab').with( { 'name' => 'gitlab' } )
+      should contain_service('gitlab').with( { 'name' => 'gitlab' } )
+    }
+  end
+
+  context "On an unknown OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'Darwin'
+      }
+    end
+
+    it {
+      expect { should raise_error(Puppet::Error) }
+    }
+  end
+
+  context "With a package name specified" do
+    let :params do
+      {
+        :package_name => 'gitlab'
+      }
+    end
+
+    it {
+      should contain_package('gitlab').with( { 'name' => 'gitlab' } )
+      should contain_service('gitlab').with( { 'name' => 'gitlab' } )
+    }
+  end
+end
