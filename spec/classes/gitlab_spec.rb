58865121f489692c123621da48cf2ce14aa6835d
Spencer Owen <owenspencer@gmail.com>
2014-07-08 18:07:13 -0600

    Moves puppet and facter to the same lines

--- spec/classes/gitlab_spec.rb
+++ spec/classes/gitlab_spec.rb
@@ -4,7 +4,7 @@ describe 'gitlab', :type => 'class' do
 
   context 'when puppet version < 3.0' do
     let(:params) { { :external_url  => 'http://gitlab.example.com', :gitlab_branch => '7.0.0'} }
-    let(:facts) { { :puppetversion => '2.7.0' , :facterversion => '2.1.0'}}
+    let(:facts) { { :puppetversion => '2.7.0' , :facterversion => ENV['FACTER_VERSION'] }}
     it 'we fail' do
       expect { subject }.to raise_error(/Gitlab requires puppet 3.0.0 or greater/)
     end
@@ -30,7 +30,6 @@ describe 'gitlab', :type => 'class' do
   context 'failure to add $gitlab_branch' do
     let(:params) { { :external_url  => 'http://gitlab.example.com'} }
     let(:facts)  { { :puppetversion => ENV['PUPPET_VERSION'], :facterversion => ENV['FACTER_VERSION'] } }
-
     it 'we fail' do
       # expect { subject }.to raise_error(/Only RedHat and Debian os families are supported/)
       expect { subject }.to raise_error(/gitlab_branch parameter required/)
@@ -40,7 +39,6 @@ describe 'gitlab', :type => 'class' do
   context 'failure to add $external_url' do
     let(:params) { { :gitlab_branch => '7.0.0'} }
     let(:facts)  { { :puppetversion => ENV['PUPPET_VERSION'], :facterversion => ENV['FACTER_VERSION'] } }
-
     it 'we fail' do
       # expect { subject }.to raise_error(/Only RedHat and Debian os families are supported/)
       expect { subject }.to raise_error(/external_url parameter required/)
