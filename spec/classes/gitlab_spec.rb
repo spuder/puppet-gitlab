6cd100dba98fa0264971a341a386e0e7cca9960f
Spencer Owen <owenspencer@gmail.com>
2014-07-09 10:23:44 -0600

    Adds spec test for gitlab::backup

--- spec/classes/gitlab_spec.rb
+++ spec/classes/gitlab_spec.rb
@@ -22,7 +22,6 @@ describe 'gitlab', :type => 'class' do
     let(:params) { { :external_url  => 'http://gitlab.example.com', :gitlab_branch => '7.0.0'} }
     let(:facts)  { { :osfamily      => 'Unsupported', :puppetversion => ENV['PUPPET_VERSION'], :facterversion => ENV['FACTER_VERSION'] } }
     it 'we fail' do
-      # expect { subject }.to raise_error(/Only RedHat and Debian os families are supported/)
       expect { subject }.to raise_error(/Only Centos, Ubuntu and Debian presently supported/)
     end
   end
@@ -31,7 +30,6 @@ describe 'gitlab', :type => 'class' do
     let(:params) { { :external_url  => 'http://gitlab.example.com'} }
     let(:facts)  { { :puppetversion => ENV['PUPPET_VERSION'], :facterversion => ENV['FACTER_VERSION'] } }
     it 'we fail' do
-      # expect { subject }.to raise_error(/Only RedHat and Debian os families are supported/)
       expect { subject }.to raise_error(/gitlab_branch parameter required/)
     end
   end
@@ -40,10 +38,50 @@ describe 'gitlab', :type => 'class' do
     let(:params) { { :gitlab_branch => '7.0.0'} }
     let(:facts)  { { :puppetversion => ENV['PUPPET_VERSION'], :facterversion => ENV['FACTER_VERSION'] } }
     it 'we fail' do
-      # expect { subject }.to raise_error(/Only RedHat and Debian os families are supported/)
       expect { subject }.to raise_error(/external_url parameter required/)
     end
   end
 
+  # context 'warning if not using Ubuntu 12.04 CentOS 6.5, Debian 7.5 ' do
+  #   let(:params) { 
+  #     { 
+  #       :gitlab_branch => '7.0.0',
+  #       :external_url  => 'http://gitlab.example.com'
+  #     }
+  #   }
+  #   let(:facts) {
+  #       {
+  #       :puppetversion => ENV['PUPPET_VERSION'], 
+  #       :facterversion => ENV['FACTER_VERSION'],
+  #       :osfamily => 'RedHat',
+  #       :operatingsystem => 'CentOS',
+  #       :operatingsystemrelease => '6.4'
+  #       }
+  #     }
+  #   it 'we fail' do
+  #     # expect { subject }.to raise_error(Puppet::Warn, /is not on approved list,/)
+  #     expect { should compile }.to raise_error(Puppet::Error, /is not on approved list,/)
+  #   end
+  # end
+
+  context 'when $puppet_manage_backups is true' do
+    let(:params) { 
+      { 
+        :gitlab_branch => '7.0.0',
+        :external_url  => 'http://gitlab.example.com',
+        :puppet_manage_backups => true,
+      }
+    }
+    let(:facts) {
+      {
+        :puppetversion => ENV['PUPPET_VERSION'], 
+        :facterversion => ENV['FACTER_VERSION'],
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+        :operatingsystemrelease => '6.5'
+      }
+    }
+    it { should contain_class('gitlab::backup') }
+  end
 
 end
