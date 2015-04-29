295416797b222b468a714bad5fc9dd9c0f92dda3
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-04-29 11:44:55 +0100

    Parameterise this class to allow the cron time to be customised

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,8 +1,8 @@
 # == Class: gitlab::backup
 #
-#  Optional Class, creates cron job to backup gitlab at 2 am every night.
-#  Set $gitlab_manage_backups to 'false' in class declaration if you wish to 
-#  manually manage backups
+#  Optional Class, creates cron job to backup gitlab. The time can be
+#  specified with a set of variables. Set $gitlab_manage_backups to
+#  'false' in class declaration if you wish to manually manage backups.
 #
 # === Parameters
 #
@@ -23,14 +23,25 @@
 #
 # Copyright 2014 Spencer Owen, unless otherwise noted.
 #
-class gitlab::backup inherits ::gitlab {
+class gitlab::backup inherits ::gitlab (
+  $backup_hour     = 2,
+  $backup_minute   = 0,
+  $backup_month    = undef,
+  $backup_day      = undef,
+  $backup_monthday = undef,
+  $backup_weekday  = undef,
+) {
 
-  # Execute rake backup every night at 2 am
+  # Execute rake backup
   cron { 'gitlab-backup':
-    command => 'CRON=1 /opt/gitlab/bin/gitlab-rake gitlab:backup:create',
-    user    => root,
-    hour    => 2,
-    minute  => 0,
+    command  => 'CRON=1 /opt/gitlab/bin/gitlab-rake gitlab:backup:create',
+    user     => root,
+    hour     => $backup_hour,
+    minute   => $backup_minute,
+    month    => $backup_month,
+    day      => $backup_day,
+    monthday => $backup_monthday,
+    weekday  => $backup_weekday,
   }
 
 }
